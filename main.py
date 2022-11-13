989aff7033ba7031a52d62c1d861ae0079f2dc15
rodolfosantos <rsantos.professional@gmail.com>
2022-11-13 18:54:37 +0000

    make snake - correction

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-snake = []
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
-for i in range(0, 3):
-    new_snake = Turtle("square")
-    new_snake.color("white")
-    new_snake.goto(x=0 - (20 * i), y=0)
-    snake.append(new_snake)
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 screen.exitonclick()
