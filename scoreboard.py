2e414a21703b89cf6fc99d95c5e7eb6720c89697
rodolfosantos <rsantos.professional@gmail.com>
2022-11-14 21:59:08 +0000

    make snake extend

--- scoreboard.py
+++ scoreboard.py
@@ -16,9 +16,13 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    def game_over(self):
+        self.home()
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
